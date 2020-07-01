c533eb9fb32de21bbb9baa1646afc69fa22f524f
Pablo Galve <eduinvestor100@gmail.com>
2020-07-01 18:55:05 +0200

    number colors in market pro + delete unused code

--- exchange/invest/includes/display_market_data.inc.php
+++ exchange/invest/includes/display_market_data.inc.php
@@ -96,16 +96,21 @@ function display_market_data($conn, $ticker, $type){  //$type= bid, ask or lastT
     }
 
     while($row = mysqli_fetch_assoc($result)){
+      //We setup the color
+      if($type == 'last_trades'){
+        if($row['type'] == 'buy')$style = "bid_price";
+        else if($row['type'] == 'sell')$style = "ask_price";
+      }
+
+      //We display ( Price | Amount | Value )
       $value = $row['amount_RP']*$row['price'];
       echo "<tr><td><div class='$style'>". 
       round($row['price'], 2, PHP_ROUND_HALF_EVEN) . "</div></td> 
       <td>". round($row['amount_RP'], 2, PHP_ROUND_HALF_EVEN) ." </td>
       <td>" . round($value, 2, PHP_ROUND_HALF_EVEN) . "€</td>";
 
+      //We display ( Type | Date ) only if it's "last trades"
       if($type == 'last_trades'){
-        if($row['type'] == 'buy')$style = "bid_price";
-        else if($row['type'] == 'sell')$style = "ask_price";
-
         echo "<td><div class='$style'>". $row['type'] ."</div></td>
         <td>". $row['date'] ."</td></tr>";
       }
@@ -114,124 +119,3 @@ function display_market_data($conn, $ticker, $type){  //$type= bid, ask or lastT
     echo '</table>';
   }
 ?>
-
-
-<?php
-  if($ticker == 'mfeur'){
-        if($type == 'lastTrades'){
-?>
-            <html>
-            <head>
-            <style>
-            table, th, td {
-                border: 1px solid black;
-                border-collapse: collapse;
-                padding: 5px;
-                text-align: left;
-            }
-            </style>
-            </head>
-            <body>
-    
-            <table style="width:100%">
-              <tr>
-                <th>Username</th>
-                <th>Amount(MF)</th>
-                <th>Price(EUR)</th>
-                <th>Type</th>
-                <th>Fee(EUR)</th>
-                <th>Fee(Royalty)</th>
-                <th>Date</th>
-              </tr>
-            
-            <?php
-                $sql = "SELECT * FROM mfeurtrades ORDER BY date DESC LIMIT 20";
-                $result = $conn->query($sql);
-                echo "<br><br><br> Last Trades <br>";
-                
-                while($row = mysqli_fetch_assoc($result)){
-                    echo "<tr><td>". $row['username'] ."</td><td>". $row['amountRP'] ."</td><td>". $row['price'] ."</td><td>". $row['orderType'] ."</td><td>". $row['feeEUR'] ."</td><td>". $row['feeRP'] ."</td><td>". $row['date'] ."</td></tr>";
-                }
-                echo '</table>';
-            ?>
-            </table>
-            </body>
-            </html>
-            <?php
-        }else if($type == 'bid'){
-            ?>
-            <html>
-            <head>
-            <style>
-            table, th, td {
-                border: 1px solid black;
-                border-collapse: collapse;
-                padding: 5px;
-                text-align: left;
-            }
-            </style>
-            </head>
-            <body>
-    
-            <table style="width:100%">
-              <tr>
-                <th>Username</th>
-                <th>Amount(MF)</th>
-                <th>Price(EUR)</th>
-              </tr>
-            
-            <?php
-
-            $sql = "SELECT * FROM mfeurbids ORDER BY price DESC LIMIT 10";
-            $result = $conn->query($sql);
-            echo "<br><br><br> Buy Orders <br>";
-            
-            while($row = mysqli_fetch_assoc($result)){
-                echo "<tr><td>". $row['username'] ."</td><td>". $row['amountRP'] ."</td><td>". $row['price'] ."</td></tr>";
-            }
-            echo '</table>';
-            ?>
-            </table>
-            </body>
-            </html>
-            <?php
-        }else if($type == 'ask'){
-            ?>
-            <html>
-            <head>
-            <style>
-            table, th, td {
-                border: 1px solid black;
-                border-collapse: collapse;
-                padding: 5px;
-                text-align: left;
-            }
-            </style>
-            </head>
-            <body>
-    
-            <table style="width:100%">
-              <tr>
-                <th>Username</th>
-                <th>Amount(MF)</th>
-                <th>Price(EUR)</th>
-              </tr>
-            
-            <?php
-
-            $sql = "SELECT * FROM mfeurasks ORDER BY price DESC LIMIT 10";
-            $result = $conn->query($sql);
-            echo "<br><br><br> Sell Orders <br>";
-            
-            while($row = mysqli_fetch_assoc($result)){
-                echo "<tr><td>". $row['username'] ."</td><td>". $row['amountRP'] ."</td><td>". $row['price'] ."</td></tr>";
-            }
-            echo '</table>';
-            ?>
-            </table>
-            </body>
-            </html>
-            <?php
-        }
-    }
-}
