6b6b1fe94280031d095c667cb5df8fe8b5ebad0c
pablogalve <pablogalve100@gmail.com>
2021-08-24 12:37:13 +0200

    Update about-us.php

--- exchange/about-us.php
+++ exchange/about-us.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html>
-<title>About Us</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="about-us-style.css">
+    <head>
+        <title>About Us</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="about-us-style.css">
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108280184-2"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-108280184-2');
+        </script>
+    </head>
 
 <body>
 
@@ -14,11 +26,11 @@
 
 <div class="about-section">
     <h1>About Us</h1>
-    <p>Caramel Capital is an investment firm focused on good opportunities.</p>
-    <p>We aim to increase our revenue while leaving a possitive impact in the world, that's why we back projects that empower their employees and the ecosystem.</p>
+    <p>Caramel Capital is a real-time marketplace to buy and sell stocks.</p>
 </div>
 
 <h2 style="text-align:center">Our Team</h2>
+<p style="text-align:center">If you want to be listed here you can contribute in our <a href="https://github.com/pablogalve/Caramel-Trading-Engine">github repository.</a></p> <p style="text-align:center">After you have contributed, you can send an email to <a href="mailto:pablogalve100@gmail.com">pablogalve100@gmail.com</a> and you will be included.</p>
 
 <div class="row">
     <div class="column">
@@ -26,38 +38,13 @@
             <img src="images/pablogalve.PNG" alt="Pablo" class="profile_pic">
             <div class="container">
                 <h2>Pablo Galve Millán</h2>
-                <p class="title">Developer</p>
-                <p>I developed this entire website, from design to trading engine algorithms.</p>
+                <p class="title">Software Developer</p>
+                <p>I started this project. I am responsible of the coding of the trading engine (back-end) and the visual design (front-end)</p>
                 <p><i class="fa fa-envelope"></i> pablogalve100@gmail.com</p>
                 <a href="https://www.linkedin.com/in/pablogalve/" target="_blank"><i class="fa fa-linkedin"></i> Pablo Galve Millán</a>
-                <p><button class="button">I'm here to help you!</button></p>
+                <p><a href="https://www.pablogalve.com" class="button">I'm here to help you!</a></p>
             </div>
         </div>
     </div>
-    <div class="column">
-        <div class="card">
-            <img src="images/pablogalve.PNG" alt="Pablo" class="profile_pic">
-            <div class="container">
-                <h2>Pablo Galve</h2>
-                <p class="title">Developer</p>
-                <p>I developed this entire website, from design to trading engine algorithms.</p>
-                <p></p><i class="fa fa-envelope"></i> pablogalve100@gmail.com</p>
     
-                <p><button class="button">Contact</button></p>
-            </div>
-        </div>
-    </div>
-    <div class="column">
-        <div class="card">
-            <img src="images/pablogalve.PNG" alt="Pablo" class="profile_pic">
-            <div class="container">
-                <h2>Pablo Galve</h2>
-                <p class="title">Developer</p>
-                <p>I developed this entire website, from design to trading engine algorithms.</p>
-                <p></p><i class="fa fa-envelope"></i> pablogalve100@gmail.com</p>
-    
-                <p><button class="button">Contact</button></p>
-            </div>
-        </div>
-    </div>
 </div>
